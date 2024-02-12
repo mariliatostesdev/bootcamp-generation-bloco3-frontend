5c53533b6bc25b01886bd00a08beaa8f91f5ac6b
mariliatostesdev <mahtostes.dev@gmail.com>
2024-02-12 10:46:06 -0300

    updated with JS

---
+++ fundamentos-web/assets/js/script.js
@@ -0,0 +1,36 @@
+let nome = document.getElementById('name');
+let email = document.querySelector('#email');
+let nomeOk = false;
+let emailOk = false;
+
+function validaNome() {
+	let txtNome = document.getElementById('txtNome');
+	if (nome.value.length < 3) {
+		txtNome.innerHTML = 'Nome inválido';
+		txtNome.style.color = 'red';
+	} else {
+		txtNome.innerHTML = 'Nome válido';
+		txtNome.style.color = 'green';
+		nomeOk = true;
+	}
+}
+
+function validaEmail() {
+	let txtEmail = document.getElementById('txtEmail');
+	if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+		txtEmail.innerHTML = 'Email inválido';
+		txtEmail.style.color = 'red';
+	} else {
+		txtEmail.innerHTML = 'Email válido';
+		txtEmail.style.color = 'green';
+		emailOk = true;
+	}
+}
+
+function enviar() {
+	if (nomeOk && emailOk) {
+		alert('Formulário enviado com sucesso!');
+	} else {
+		alert('Preencha o formulário corretamente.');
+	}
+}
