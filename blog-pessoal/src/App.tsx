4fe60368c56d2901ee6533e0474a3df077a9678a
mariliatostesdev <mahtostes.dev@gmail.com>
2024-02-14 13:06:01 -0300

    first commit

---
+++ blog-pessoal/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './paginas/home/Home';
+import './App.css';
+
+function App() {
+	return (
+		<Home
+			title="Componente Home"
+			description="Este é um componente Home que recebe props."
+		/>
+	);
+}
+
+export default App;
