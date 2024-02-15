3f37514d0d612fda67b04d19ad3696201180e461
mariliatostesdev <mahtostes.dev@gmail.com>
2024-02-15 13:54:32 -0300

    updated with react hook

--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './Home.css';
+import React, { useState, useEffect } from 'react';
 
-function Home() {
+interface minhaProps {
+	title: string;
+	description: string;
+}
+
+function Home(props: minhaProps) {
 	return (
 		<>
-			<h1 className="titulo">Home</h1>
-			<img
-				src="https://i.imgur.com/H88yIo2.png"
-				alt="Imagem Tela Inicial"
-				className="img"
-			/>
+			<h2>{props.title}</h2>
+			<p>{props.description}</p>
 		</>
 	);
 }
