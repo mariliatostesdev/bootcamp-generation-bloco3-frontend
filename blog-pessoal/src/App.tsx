b62bf707e7a029607e9f0f45959e2b93fcc8fdaf
mariliatostesdev <mahtostes.dev@gmail.com>
2024-02-16 15:36:37 -0300

    first commit

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,25 +1,15 @@
-import Home from './paginas/home/Home';
 import './App.css';
-import React, { useState } from 'react';
+import Home from './paginas/home/Home';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 function App() {
-	const [valor, setValor] = useState(0);
-
-	function handleClick() {
-		setValor(valor + 1);
-	}
-
 	return (
-		// <Home
-		// 	title="Componente Home"
-		// 	description="Este é um componente Home que recebe props."
-		// />
-
-		<div>
-			<p>O valor é: {valor}</p>
-			<button onClick={handleClick}>Adicionar 1</button>
-		</div>
+		<>
+			<Navbar />
+			<Home />
+			<Footer />
+		</>
 	);
 }
-
 export default App;
