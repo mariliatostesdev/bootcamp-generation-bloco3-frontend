2528d831bb3d7e22ea15d46bbbbb04d04922f640
mariliatostesdev <mahtostes.dev@gmail.com>
2024-02-27 09:57:56 -0300

    added toast alerts

--- blog-pessoal/src/components/navbar/Navbar.tsx
+++ blog-pessoal/src/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
+import { toastAlerta } from '../../utils/toastAlerta';
 
 function Navbar() {
 	let navigate = useNavigate();
@@ -9,14 +10,14 @@ function Navbar() {
 
 	function logout() {
 		handleLogout();
-		alert('Usuário deslogado com sucesso');
+		toastAlerta('Usuário deslogado com sucesso', 'info');
 		navigate('/login');
 	}
 
 	let navbarComponent;
 
-	return (
-		<>
+	if (usuario.token !== '') {
+		navbarComponent = (
 			<div className="w-full bg-indigo-900 text-white flex justify-center py-4">
 				<div className="container flex justify-between text-lg">
 					<Link to="/home" className="text-2xl font-bold uppercase">
@@ -33,15 +34,19 @@ function Navbar() {
 						<Link to="/cadastroTema" className="hover:underline">
 							Cadastrar tema
 						</Link>
-						<div className="hover:underline">Perfil</div>
+						<Link to="/perfil" className="hover:underline">
+							Perfil
+						</Link>
 						<Link to="" onClick={logout} className="hover:underline">
 							Sair
 						</Link>
 					</div>
 				</div>
 			</div>
-		</>
-	);
+		);
+	}
+
+	return <>{navbarComponent}</>;
 }
 
 export default Navbar;
--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import homeLogo from '../../assets/home.png';
-import './Home.css';
 import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 
